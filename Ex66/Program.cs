6705ff7133aad054fb981116f376fc924228c301
KreoX <soldimksol@gmail.com>
2022-09-06 19:54:41 +0300

    Ex66

--- Ex66/Program.cs
+++ Ex66/Program.cs
@@ -7,8 +7,8 @@
 
 int SumNum(int start, int end, int sum)
 {
-    if (start == end) return sum+start;
-    else return SumNum(start+1,end, sum+start);
+    if (start == end) return sum + start;
+    else return SumNum(start + 1, end, sum + start);
 }
 
 int start = Prompt("Введите число M:");
@@ -17,9 +17,9 @@ int end = Prompt("Введите число N:");
 
 if (start <= end)
 {
-    Console.WriteLine(SumNum(start, end,0));
+    Console.WriteLine(SumNum(start, end, 0));
 }
 else
 {
-    Console.WriteLine(SumNum(end, start,0));
+    Console.WriteLine(SumNum(end, start, 0));
 }
