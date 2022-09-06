8ad53349cf69db7f3c0bfbe12b9f924356d100f7
KreoX <soldimksol@gmail.com>
2022-09-06 20:13:04 +0300

    Ex68

---
+++ Ex68/Program.cs
@@ -0,0 +1,30 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if ((m != 0) && (n == 0))
+        {
+            return Akkerman(m - 1, 1);
+        }
+        else
+        {
+            return Akkerman(m - 1, Akkerman(m, n - 1));
+        }
+    }
+}
+
+int M = Prompt("Введите число M:");
+int N = Prompt("Введите число N:");
+
+Console.WriteLine(Akkerman(N, M));
